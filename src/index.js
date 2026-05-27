90f4e123affaef559188c459f5162c7910ee1645
Dipaque <rajadeepak1989@gmail.com>
2026-05-28 01:12:03 +0530

    fix: encapsulated classes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
+
 const AdvanceRetrySystem = require("./classes/AdvanceRetrySystem");
-const CircuitBreaker = require("./classes/CircuitBreaker");
-const ExponentialBackoff = require("./classes/ExponentialBackoff");
 
 module.exports = {
     AdvanceRetrySystem,
-    CircuitBreaker,
-    ExponentialBackoff,
 };
